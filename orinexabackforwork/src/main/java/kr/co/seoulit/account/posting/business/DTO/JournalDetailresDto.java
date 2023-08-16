1cf4bff86868085b8dc20358ff00d9cf00083f8e
meoooooooooooooow <sck07198@naver.com>
2023-08-16 17:08:57 +0900

    posting/business/DTO에서 reqDto와 resDto로 수정

--- orinexabackforwork/src/main/java/kr/co/seoulit/account/posting/business/DTO/JournalDetailresDto.java
+++ orinexabackforwork/src/main/java/kr/co/seoulit/account/posting/business/DTO/JournalDetailresDto.java
@@ -1,6 +1,17 @@
 package kr.co.seoulit.account.posting.business.DTO;
 
 import kr.co.seoulit.account.sys.base.to.BaseBean;
+import kr.co.seoulit.account.sys.common.annotation.Dataset;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Dataset(name = "gds_journalDetail")
+@Getter
+@Setter
 public class JournalDetailresDto extends BaseBean {
+    private String journalDetailNo;
+    private String journalNo;
+    private String accountControlDescription;
 }
